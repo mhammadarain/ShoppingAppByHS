339ef2089a945c0c928cece16176228883ee5436
Hammad Arain <mhammadarain11@gmail.com>
2023-08-18 01:19:31 +0500

    Day 5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mart_app/pages/home_page.dart';
 import 'package:mart_app/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mart_app/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,14 +14,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         // home: HomePage(),
         themeMode: ThemeMode.light,
-        darkTheme: ThemeData(brightness: Brightness.dark),
-        initialRoute: "/Home",
+        darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            fontFamily: GoogleFonts.lato().fontFamily),
+        initialRoute: "/Login",
         routes: {
           "/": (context) => LoginPage(),
-          "/Login": (context) => LoginPage(),
-          "/Home": (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
         });
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mart_app/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,47 +8,54 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/images/pic1.png",
-            fit: BoxFit.cover,
-          ),
-          const SizedBox(
-            height: 22,
-          ),
-          const Text(
-            "Welcome",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 22,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter Username",
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                ),
-                const SizedBox(
-                  height: 22,
-                ),
-                ElevatedButton(onPressed: () {}, child: Text("Login"))
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/pic1.png",
+              fit: BoxFit.cover,
+              height: 300,
             ),
-          )
-        ],
+            const SizedBox(
+              height: 22,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 22,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 44,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text("Login"))
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
